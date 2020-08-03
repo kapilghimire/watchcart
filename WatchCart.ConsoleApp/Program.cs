b6ee0e3eaf5339374db692257012c6e5ed50e616
github_Manish <m.karn2011@gmail.com>
2020-08-03 09:29:51 +0530

    console welcome message

--- WatchCart.ConsoleApp/Program.cs
+++ WatchCart.ConsoleApp/Program.cs
@@ -13,14 +13,20 @@ namespace WatchCart.ConsoleApp
             /*
              *  Prepare the instance of all dependencies and inject into respective objects.
              */
-            
+
             // It should display a welcome message to the user followed by the options to select 
             // different criterias.
-           
+
             // Display the details of matched watch with the help of ToString() method
             // Ask the user to confirm about the purchase of watch
             // 1. Yes -> Save the orderdetails into database using OrderRepository
             // 2. No -> Should exit the application
+
+            Console.WriteLine("Welcome To Our Store!");
+            Console.WriteLine("Do You want to continue");
+            Console.Read();
+
+
         }    
     }
 }
